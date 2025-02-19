bbd708fa86915ed70913e7438387ddefd3d60aff
Pablo Vargas <49590991+PabloVarg@users.noreply.github.com>
2025-02-18 19:43:24 -0600

    feat(navbar): Implement navbar

---
+++ src/constants/navbar/navbar.tsx
@@ -0,0 +1,10 @@
+import { NavItem } from "@/components/NavBar/NavBar";
+import { SiGooglehome } from "react-icons/si";
+
+export const navItems: NavItem[] = [
+  {
+    name: "Home",
+    link: "#hero",
+    icon: <SiGooglehome />,
+  },
+];
