a5e126f0d674e3137fe54987c4bddb4ae6449619
Pablo Vargas <49590991+PabloVarg@users.noreply.github.com>
2025-02-19 20:19:15 -0600

    feat(home): Implement about me section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import MainButton from "@/components/Buttons/MainButton";
 import { InfiniteMovingCards } from "@/components/Carousels/InfiniteScroll";
 import MeteorsHero from "@/components/MeteorsHero/meteors";
+import FlipWords from "@/components/Text/FlipText";
 import { TextGenerateEffect } from "@/components/Text/GenerateText";
 import { Timeline } from "@/components/Timeline/Timeline";
 import { companies } from "@/constants/companies/companies";
@@ -14,6 +15,7 @@ export default function Home() {
     <main>
       <Hero />
       <Companies />
+      <AboutMe />
       <Experience />
     </main>
   );
@@ -67,5 +69,41 @@ function Experience() {
       description="A journey through my professional and academic milestones, highlighting key projects, roles, and achievements that have shaped my growth as a backend-focused software engineer."
       data={timelineItems}
     />
+  );
+}
+
+function AboutMe() {
+  return (
+    <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 place-items-center gap-8 py-16">
+      <Image
+        className="mb-4 rounded-3xl border-gold transition-shadow"
+        src="/images/photos/avatar-suit.webp"
+        alt="Pablo Vargas Bermudez"
+        width={640 * 0.5}
+        height={640 * 0.5}
+      />
+
+      <div className="text-lg">
+        <p className="mb-4">
+          I&apos;m a software engineer specializing in backend development, with
+          a strong focus on <span className="text-light-gold">Go</span> and{" "}
+          <span className="text-light-gold">Python</span>.
+        </p>
+        <p className="mb-8">
+          I graduated with honors from Tecnológico Nacional de México (TecNM)
+          with a Bachelor&apos;s degree in Computer Systems Engineering as{" "}
+          <span className="text-light-gold">top of my generation</span>.
+        </p>
+
+        <span className="text-3xl overflow-hidden">
+          I&apos;m deeply passionate about
+          <br />
+          <FlipWords
+            words={["Algorithms", "Distributed Systems", "Backend"]}
+            className="text-light-gold"
+          />
+        </span>
+      </div>
+    </div>
   );
 }
