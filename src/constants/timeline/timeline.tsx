d4b1f83d962fc5e50e0dbfd1d13003eff3a74d44
Pablo Vargas <49590991+PabloVarg@users.noreply.github.com>
2025-02-19 20:26:49 -0600

    feat(timeline): Update TecNM timeline entry

--- src/constants/timeline/timeline.tsx
+++ src/constants/timeline/timeline.tsx
@@ -43,4 +43,36 @@ export const timelineItems: TimelineEntry[] = [
       </div>
     ),
   },
+  {
+    title: (
+      <Link target="_blank" href="https://www.tecnm.mx/">
+        <Image
+          src="/images/companies/tecnm.webp"
+          alt="TecNM"
+          width={250}
+          height={50}
+        />
+      </Link>
+    ),
+    content: (
+      <div>
+        During my tenure at Tecnológico Nacional de México (TecNM), I focused on
+        optimizing an application responsible for generating reports based on
+        survey data collected from students to evaluate teachers&apos;
+        performance. Utilizing my SQL expertise, I implemented optimizations
+        within the application to enhance its performance. By fine-tuning SQL
+        queries and database interactions, I was able to significantly improve
+        the speed and efficiency of report generation. Working closely with the
+        development team, I collaborated on identifying bottlenecks and
+        implementing strategic improvements to ensure the application delivered
+        accurate and timely reports. These optimizations not only streamlined
+        the reporting process but also provided valuable insights into
+        teachers&apos; performance and contributed to the overall improvement of
+        educational quality. My experience at Tecnológico Nacional de México
+        (TecNM) allowed me to apply my technical skills in a practical setting,
+        optimize performance, and make a positive impact on the
+        institution&apos;s evaluation processes.
+      </div>
+    ),
+  },
 ];
